aa9cc7ec0f4a146b2a31b4e579170e727535d9f2
nguyenthucphuong <131501734+nguyenthucphuong@users.noreply.github.com>
2023-05-07 18:36:40 +0700

    Update Program.cs
    Edit

--- Bai_7/Program.cs
+++ Bai_7/Program.cs
@@ -14,7 +14,7 @@ namespace Bai_7
             while (true)
             {
                 Console.Write("Nhập số nguyên n > 0, n = ");
-                if ((int.TryParse(Console.ReadLine(), out n) && n > 0)) break;
+                if (int.TryParse(Console.ReadLine(), out n) && n > 0) break;
                 Console.Write("Không hợp lệ!");
             }
             for (int i = 1; i <= n; i += 2)
@@ -23,7 +23,6 @@ namespace Bai_7
             }
             Console.WriteLine("Tổng các số lẻ từ 1 đến {0} là: {1}", n, sum);
             Console.ReadKey();
-
         }
     }
 }
